0acb24e28beb6ff09096fa2db94b79c85d2107a8
Ignacio Ivan Cossio Roncal <2788936@gmail.com>
2025-12-02 13:54:51 -0400

    Header implemented and website sctructure made

---
+++ app/properties/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Properties() {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white pt-32 px-8">
+      <h1 className="text-4xl font-bold">Properties</h1>
+      <p className="mt-4">Browse our available properties</p>
+    </div>
+  )
+}
